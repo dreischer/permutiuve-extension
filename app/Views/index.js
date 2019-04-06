2ea08d66ddb68c11ac7c8010d926629ccac34599
David Reischer <reischer.david@gmail.com>
2019-04-06 11:45:03 +0100

    move wrapper into App file

--- app/Views/index.js
+++ app/Views/index.js
@@ -2,8 +2,6 @@ import Events from './Events.js'
 import Segments from './Segments.js'
 import GoogleAM from './GoogleAM.js'
 
-import './view.css'
-
 export {
   Events, Segments, GoogleAM
 }
--- app/app.js
+++ app/app.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'preact'
 import createAtom from 'tiny-atom'
-import { Provider } from 'tiny-atom/preact'
-import Wrapper from './Views/Wrapper'
+import { Provider, Consumer } from 'tiny-atom/preact'
+import Header from './components/Header'
+import Icon from './components/Icon'
+import Menu from './components/Menu'
 
 import './index.css'
 
@@ -25,8 +27,73 @@ export default class App extends Component {
   render () {
     return (
       <Provider atom={atom}>
-        <Wrapper atom={atom} />
+        <Inner atom={atom} />
       </Provider>
+    )
+  }
+}
+
+class Inner extends Component {
+  componentDidMount () {
+    const { atom } = this.props
+    atom.actions.updateSegments()
+    window.parent.addEventListener('message', atom.actions.addDfp)
+    const allEvents = window.parent.permutive.on(/.*/, atom.actions.addEvent).replay()
+    const segmentEvents = window.parent.permutive.on(/Segment(Entry|Exit)/, atom.actions.updateSegments)
+    window.eventListeners = window.eventListeners || []
+    window.eventListeners = [allEvents, segmentEvents]
+  }
+  componentWillUnmount () {
+    const { atom } = this.props
+    window.eventListeners.forEach(listener => listener.remove())
+    window.parent.removeEventListener('message', atom.actions.addDfp)
+  }
+  render (props, state) {
+    const map = function (state) {
+      const { open, navigation, data } = state
+      return {
+        open,
+        content: navigation.items.find(item => item.id === navigation.activeItem).component,
+        activeItem: navigation.activeItem,
+        items: navigation.items,
+        counts: {
+          events: data.events.count,
+          segments: data.segments.count,
+          googleAM: data.googleAM.count
+        }
+      }
+    }
+    const actions = ['toggleOpen', 'setMenuLocation']
+
+    const icon = (
+      <Consumer map={map} actions={actions} >
+        {({ counts, toggleOpen }) => (
+          <Icon counts={counts} toggle={toggleOpen} />
+        )}
+      </Consumer>
+    )
+    const widget = (
+      <Consumer map={map} actions={actions} >
+        {({ content, activeItem, items, counts, toggleOpen, setMenuLocation }) => (
+          <div>
+            <Header toggle={toggleOpen} />
+            <div class='content'>
+              <Menu items={items} activeItem={activeItem} counts={counts} onChange={setMenuLocation} />
+              { content }
+            </div>
+          </div>
+        )}
+      </Consumer>
+    )
+
+    return (
+      <Consumer map={map} >
+        {({ open }) => (
+          <div id='app'>
+            {open ? widget : icon}
+          </div>
+        )}
+      </Consumer>
     )
   }
 }
