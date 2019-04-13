e1c3af85ba1b1853da12394b2e51122a7a185e68
David Reischer <reischer.david@gmail.com>
2019-04-13 17:00:34 +0100

    remove unnecessary webpack dev server config

--- webpack.config.js
+++ webpack.config.js
@@ -18,12 +18,7 @@ module.exports = {
     https: true,
     port: 9000,
     contentBase: './html',
-    historyApiFallback: true,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
-    }
+    historyApiFallback: true
   },
   resolve: {
     alias: {
